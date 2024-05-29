1455d1b0c9113c8c5d349ef117b76d9a675b5763
Peter Kuczera <peter@integrationos.com>
2024-05-29 19:11:59 -0400

    List files

--- src/app/ssr/page.tsx
+++ src/app/ssr/page.tsx
@@ -1,15 +1,23 @@
 const { readFileSync } = require('fs')
 const { family, version, familySync, GLIBC, MUSL } = require('detect-libc');
 const bcrypt = require('bcrypt')
-
+import { glob, globSync, globStream, globStreamSync, Glob } from 'glob'
 
 const Home = async() => {
   const randomUuid = (await fetch('https://www.uuidtools.com/api/generate/v4', {cache: 'no-store'})).json()
   const detectedFamily = await family()
   const detectedVersion = await version()
 
+  const plaintextPassword = "password"
   const saltRounds = 10;
-  const salt = bcrypt.genSaltSync(saltRounds);
+  const salt = bcrypt.genSaltSync(saltRounds)
+  const hashedPassword = bcrypt.hashSync(plaintextPassword, salt)
+  const filePath = `${__dirname}/../../../..`
+  const filesystem = glob(`${filePath}/**`, {dot: true})
+    .then (array => array.sort( (a,b) => a.localeCompare(b)))
+    .then (array => array.map( item => item + "\n"))
+
+
 
   return (
     <div>
@@ -20,7 +28,18 @@ const Home = async() => {
           <div>detect-libc family(): { detectedFamily }</div>
           <div>detect-libc version(): { detectedVersion }</div>
         </div>
-        <div>bcrypt generated salt: { salt }</div>
+        <div>bcrypt hashed password: { hashedPassword }</div>
+        <div>&nbsp;</div>
+        <div>file path: { filePath }</div>
+        <div>&nbsp;</div>
+        <div>
+          <div>files:</div>
+          <div>
+            {
+            filesystem
+            }
+          </div>
+        </div>
       </pre>
     </div>
   );
