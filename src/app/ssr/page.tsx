7f57f322a92b2e579d1bd6ccf6585b0756b8370d
Peter Kuczera <peter@integrationos.com>
2024-05-29 16:50:59 -0400

    Remove isMusl() check

--- src/app/ssr/page.tsx
+++ src/app/ssr/page.tsx
@@ -2,20 +2,20 @@ const { readFileSync } = require('fs')
 const { family, familySync, GLIBC, MUSL } = require('detect-libc');
 
 
-function isMusl() {
-  // For Node 10
-  if (!process.report || typeof process.report.getReport !== 'function') {
-    try {
-      const lddPath = require('child_process').execSync('which ldd').toString().trim()
-      return readFileSync(lddPath, 'utf8').includes('musl')
-    } catch (e) {
-      return true
-    }
-  } else {
-    const glibcVersionRuntime = process.report.getReport().header
-    return !glibcVersionRuntime
-  }
-}
+// function isMusl() {
+//   // For Node 10
+//   if (!process.report || typeof process.report.getReport !== 'function') {
+//     try {
+//       const lddPath = require('child_process').execSync('which ldd').toString().trim()
+//       return readFileSync(lddPath, 'utf8').includes('musl')
+//     } catch (e) {
+//       return true
+//     }
+//   } else {
+//     const glibcVersionRuntime = process.report.getReport().header
+//     return !glibcVersionRuntime
+//   }
+// }
 
 const Home = async() => {
   const randomUuid = (await fetch('https://www.uuidtools.com/api/generate/v4', {cache: 'no-store'})).json()
@@ -27,7 +27,7 @@ const Home = async() => {
         <div>randomUuid: { randomUuid }</div>
         <div>&nbsp;</div>
         <div>
-          <div>isMusl(): { isMusl() ? 'true' : 'false' }</div>
+          {/* <div>isMusl(): { isMusl() ? 'true' : 'false' }</div> */}
           <div>&nbsp;</div>
           <div>detect-libc family(): { detectedFamily }</div>
         </div>
