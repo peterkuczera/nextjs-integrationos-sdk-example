900103b9d9a4537f6624c3add681fe2b69db8f58
Peter Kuczera <peter@integrationos.com>
2024-05-30 15:28:52 -0400

    Revert "Set includeWebpackPublicPath=true"
    This reverts commit 7333a3f9c154e83002daddefec1d54447c0e0c29.

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,6 @@ import os from "os"
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { dev, isServer, webpack, nextRuntime }) => {
-
-    // BigInt.prototype["toJSON"] = function () {
-    //   return this.toString();
-    // };
-    // console.log(`config: ${JSON.stringify(config.output, null, 2)}`)
-
     config.module.rules.push({
       test: /\.node$/,
       use: [
@@ -16,8 +10,7 @@ const nextConfig = {
           loader: "nextjs-node-loader",
           options: {
             flags: os.constants.dlopen.RTLD_NOW,
-            outputPath: config.output.path,
-            includeWebpackPublicPath: true
+            outputPath: config.output.path
           }
         },
       ],
