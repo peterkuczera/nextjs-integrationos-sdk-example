333a9eff28a7184c57576d3ea2de7c7951ba670a
Peter Kuczera <peter@integrationos.com>
2024-05-30 11:10:35 -0400

    List filesystem on static page

--- src/app/ssg/page.tsx
+++ src/app/ssg/page.tsx
@@ -1,12 +1,14 @@
-const { family, familySync, GLIBC, MUSL } = require('detect-libc');
+import { glob } from 'glob'
+
+const filesystem = glob('/**', {dot: true})
+    .then (array => array.sort( (a,b) => a.localeCompare(b)))
+    .then (array => array.map( item => item + "\n"))
 
 const Dir = async() => {
   return (
     <div>
       <pre className="whitespace-pre-line">
-        <div>
-
-        </div>
+        <div>{ filesystem }</div>
       </pre>
     </div>
   );
