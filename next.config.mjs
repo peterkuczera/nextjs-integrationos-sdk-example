7333a3f9c154e83002daddefec1d54447c0e0c29
Peter Kuczera <peter@integrationos.com>
2024-05-30 15:24:20 -0400

    Set includeWebpackPublicPath=true

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ import os from "os"
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { dev, isServer, webpack, nextRuntime }) => {
+
+    // BigInt.prototype["toJSON"] = function () {
+    //   return this.toString();
+    // };
+    // console.log(`config: ${JSON.stringify(config.output, null, 2)}`)
+
     config.module.rules.push({
       test: /\.node$/,
       use: [
@@ -10,7 +16,8 @@ const nextConfig = {
           loader: "nextjs-node-loader",
           options: {
             flags: os.constants.dlopen.RTLD_NOW,
-            outputPath: config.output.path
+            outputPath: config.output.path,
+            includeWebpackPublicPath: true
           }
         },
       ],
