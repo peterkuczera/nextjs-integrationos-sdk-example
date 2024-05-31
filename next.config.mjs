eaf0c3535ffaa4ff9984f07d01009824a1ff1a6d
Peter Kuczera <peter@integrationos.com>
2024-05-30 23:43:11 -0400

    Use regex to catch packages for all platforms and architechtures

--- next.config.mjs
+++ next.config.mjs
@@ -9,27 +9,12 @@ const nextConfig = {
       use: [
         {
           loader: "nextjs-node-loader",
-          options: {
-            flags: os.constants.dlopen.RTLD_NOW,
-            outputPath: config.output.path
-          }
         },
       ],
     });
 
-    // config.resolve.alias["@integrationos/node-darwin-arm64"] = path.resolve(
-    //   process.cwd(),
-    //   "node_modules/@integrationos/node-darwin-arm64"
-    // )
-
-    // config.resolve.alias["@integrationos/node-linux-x64-gnu"] = path.resolve(
-    //   process.cwd(),
-    //   "node_modules/@integrationos/node-linux-x64-gnu"
-    // )
-
     if (isServer) {
-      config.externals.push("@integrationos/node-darwin-arm64")
-      config.externals.push("@integrationos/node-linux-x64-gnu")
+      config.externals.push(/^@integrationos\/node-/);
     }
 
     return config
