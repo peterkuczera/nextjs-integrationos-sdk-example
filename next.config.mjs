03d5839678cb442410b16d51305a19b1999d245a
Peter Kuczera <peter@integrationos.com>
2024-05-31 00:30:14 -0400

    Remove nextjs-node-loader dependency

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,6 @@ import path from "path"
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { dev, isServer, webpack, nextRuntime }) => {
-    config.module.rules.push({
-      test: /\.node$/,
-      use: [
-        {
-          loader: "nextjs-node-loader",
-        },
-      ],
-    });
-
     if (isServer) {
       config.externals.push(/^@integrationos\/node-/);
     }
